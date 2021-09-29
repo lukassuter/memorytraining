c4d7790fce4bb1a04f6c4129c75938eeb27a2de3
Lukas Suter <lukas.suter@swegon.com>
2021-09-29 13:24:26 +0200

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import numbers from "./data/numbers1to49.json";
+const items = numbers.sort((a, b) => 0.5 - Math.random());
 
-function App() {
+export const App = () => {
+  const [index, setIndex] = useState(0);
+  const [showText, setShowText] = useState(false);
+  const [currentItem, setCurrentItem] = useState(items[index]);
+  const [errors, setErrors] = useState(0);
+  const [text, setText] = useState("");
+
+  const showNextItem = () => {
+    if (currentItem.swedishTranslation.toLowerCase() !== text) {
+      setErrors(errors + 1);
+    }
+
+    setText("");
+    setShowText(false);
+    setIndex(index + 1);
+  };
+
+  const showTextButtonPress = () => {
+    setShowText(true);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      showNextItem();
+    }
+  };
+
+  useEffect(() => {
+    if (index < items.length) {
+      setCurrentItem(items[index]);
+    }
+  }, [index]);
+
+  const lastImage = index >= items.length;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="topLevel">
+        {lastImage ? (
+          <div className="content">
+            <div className="mainContent">
+              <div className="largeText">Errors {errors}</div>
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className="content">
+              <div className="mainContent">
+                <div className="largeText">{currentItem.number}</div>
+                {showText && (
+                  <div className="largeText">
+                    {currentItem.swedishTranslation}
+                  </div>
+                )}
+              </div>
+              <input
+                type="text"
+                value={text}
+                onKeyPress={(event) => handleKeyPress(event)}
+                onChange={(event) => setText(event.target.value)}
+              ></input>
+              <div className="smallText">
+                {index + 1} of {items.length} (Errors: {errors})
+              </div>
+            </div>
+            <div className="buttonGroup">
+              <button className="primaryButton" onClick={() => showNextItem()}>
+                Next
+              </button>
+              <button
+                className="secondaryButton"
+                onClick={() => showTextButtonPress()}
+              >
+                I forgot
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
